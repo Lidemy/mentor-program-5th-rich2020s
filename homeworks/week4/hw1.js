fdd2a92236140abd9cdc74259ba9d741a95ae3b1
Rich Chan <rich@Jasmines-MacBook-Pro.local>
2021-05-07 11:33:55 +0800

    week4

---
+++ homeworks/week4/hw1.js
@@ -0,0 +1,10 @@
+const request = require('request')
+
+request('https://lidemy-book-store.herokuapp.com/books',
+  (error, response, body) => {
+    const json = JSON.parse(body)
+    for (let i = 0; i < 10; i++) {
+      console.log(`${json[i].id} ${json[i].name}`)
+    }
+  }
+)
