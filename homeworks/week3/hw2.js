41f14719277b9b53f4ad4ce14fdcebe7675ba26c
Rich Chan <rich@Jasmines-MacBook-Pro.local>
2021-05-07 18:08:50 +0800

    week3fixed

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,16 +1,15 @@
 function solve(n1, n2) {
   for (let i = n1; i <= n2; i++) {
-    if (Narcissistic(i)) console.log(i)
+    if (isNarcissistic(i)) console.log(i)
   }
 }
-function Narcissistic(n) {
-  n = n.toString()
+function isNarcissistic(n) {
+  const str = n.toString()
   let sum = 0
-  for (let i = 0; i < n.length; i++) {
-    const num = Math.pow(Number(n[i]), n.length)
+  for (let i = 0; i < str.length; i++) {
+    const num = Math.pow(Number(str[i]), str.length)
     sum += num
   }
-  if (sum === Number(n)) return true
-  else return false
+  return sum === Number(n)
 }
 solve(5, 200)
