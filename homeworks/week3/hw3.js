d2aae002ceb448481d77543361e95bceba959f45
Rich Chan <rich@Jasmines-MacBook-Pro.local>
2021-05-07 18:08:50 +0800

    week3fixed

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,8 +1,8 @@
 function isPrime(n) {
-  n = Number(n)
-  if (n === 1) return 'Composite'
-  for (let i = 2; i <= n; i++) {
-    if (n % i === 0 && i !== n) return 'Composite'
+  const num = Number(n)
+  if (num === 1) return 'Composite'
+  for (let i = 2; i <= num; i++) {
+    if (num % i === 0 && i < n) return 'Composite'
   }
   return 'Prime'
 }
