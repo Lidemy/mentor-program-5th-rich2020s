9acb2ae694c2af0d07371c3299dd81227d3a32d3
Rich Chan <rich@Jasmines-MacBook-Pro.local>
2021-05-17 13:44:40 +0800

    HTTP Challange

---
+++ homeworks/week5/newre.js
@@ -0,0 +1,25 @@
+// const request = require('request')
+// // const http = require('http')
+// // let options = {
+// //   host: "202.57.37.197",
+// //   port: 38629,
+// //   path: "https://lidemy-http-challenge.herokuapp.com/api/v3/logs",
+// // };
+// // http.get(options, function(res) {
+// //   console.log(res);
+// //   // res.pipe(process.stdout);
+// // });
+// const option = {
+//   url : 'https://lidemy-http-challenge.herokuapp.com/api/v3/index',
+//   headers:{
+//     'User-Agent':'Googlebot'
+//   }
+//     // 'User-Agent':'Mozilla/4.0 (compatible; U; MSIE 6.0; Windows NT 5.1)',
+//     // 'X-Library-Number':20,
+//     // 'origin': 'http://lidemy.com'
+// }
+// function callback(err, response, body){
+//   // const json = JSON.parse(response)
+//   console.log(body)
+// }
+// request(option, callback)
