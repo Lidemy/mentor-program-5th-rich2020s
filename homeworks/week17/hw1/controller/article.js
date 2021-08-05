094e5bc0e45552cf2dddcf5b2a6c074c2c4ae4fb
Rich Chan <rich@Jasmines-MacBook-Pro.local>
2021-08-05 11:14:00 +0800

    v.003 gitignore, 登入和文章顯示機制

--- homeworks/week17/hw1/controller/article.js
+++ homeworks/week17/hw1/controller/article.js
@@ -1,6 +1,61 @@
+const db = require('../models')
+
+const Article = db.rich_w17_article
+const User = db.rich_w17_user
 
 const articleController = {
-
+  add: (req, res) => {
+    const { title, content } = req.body
+    Article.create({
+      title,
+      content,
+      richUserId: req.session.userId
+    }).then((results) => {
+      console.log(results)
+      res.redirect('/home')
+    }).catch((err) => {
+      req.flash('messages', err.toString())
+      res.redirect('back')
+    })
+  },
+  getAll: (req, res) => {
+    Article.findAll({
+      limit: 5,
+      order: [
+        ['id', 'DESC']
+      ],
+      include: [{
+        model: User,
+        as: 'userId'
+      }]
+    }).then((results) => {
+      // console.log(results)
+      res.render('home', { results })
+    }).catch((err) => {
+      req.flash('messages', err.toString())
+      res.redirect('/login')
+    })
+  },
+  getMyarticle: (req, res) => {
+    Article.findAll({
+      where: {
+        // id: req.session.userId
+        richUserId: req.session.userId
+      },
+      order: [
+        ['id', 'DESC']
+      ],
+      include: [{
+        model: User,
+        as: 'userId'
+      }]
+    }).then((results) => {
+      res.render('manage', { results })
+    }).catch((err) => {
+      req.flash('messages', err.toString())
+      res.redirect('back')
+    })
+  }
 }
 
-module.export = articleController
+module.exports = articleController
