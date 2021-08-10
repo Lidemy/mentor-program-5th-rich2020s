38f6c7c54074fc5dcb935dca20a8daf737bbe2c8
Rich Chan <rich@Jasmines-MacBook-Pro.local>
2021-08-10 11:37:59 +0800

    部署後

--- homeworks/week17/hw1/index.js
+++ homeworks/week17/hw1/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5001
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(session({
-  secret: 'keyboard cat',
+  secret: process.env.seesionSecret,
   resave: false,
   saveUninitialized: true
 }))
